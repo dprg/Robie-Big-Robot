5cc976d6c90b8b71ca7c5cb72adf613a02b4dab2
Paul Bouchier <paul.bouchier@gmail.com>
2025-03-03 11:51:22 -0600

    initial arduino code commit

---
+++ arduino_Big_Robie/Big_robot_pgm1/movementFuncs.ino
@@ -0,0 +1,27 @@
+void tilt(motor *mtr){
+	setMotor(mtr->dir,mtr->speed,mtr);
+}
+
+
+void pan(motor *mtr) {
+	setMotor(mtr->dir,mtr->speed,mtr);
+}
+
+void tiltWithFback(int targetY,int mtrZoneBoundries[],motor *mtr) {                      // assumes targetY goes to zero as you approach target
+	if(targetY < mtrZoneBoundries[0]) {                                                  // max lift boundary
+		// set spd to max Lift value (do not know yet if cw or ccw)
+    }
+    if((targetY > mtrZoneBoundries[0]) && (targetY < mtrZoneBoundries[1])) {             // lower dead band boundary
+		// set spd to proportial Lift value (do not know yet if cw or ccw)
+    }
+	if((targetY > mtrZoneBoundries[1]) && (targetY < mtrZoneBoundries[2])) {             // upper dead band boundary
+		// set spd to dead band value (do not know yet if cw or ccw)
+    } 
+	if((targetY > mtrZoneBoundries[2]) && (targetY < mtrZoneBoundries[3])) {             // max drop boundary
+		// set spd to proportial Fall value (do not know yet if cw or ccw)
+    }
+	if(targetY > mtrZoneBoundries[3]) {                                                  // max lift boundary
+		// set spd to max Fall value (do not know yet if cw or ccw)
+    }
+}
+
---
+++ arduino_Big_Robie/Big_robot_pgm2/servoFuncs.ino
@@ -0,0 +1,20 @@
+/*************************************************
+*
+* servo related funcs
+*
+*************************************************/
+
+void wave() 
+{ 
+  for (int j = 0; j < 2; j++) {	
+	  for(int pos = 0; pos < 180; pos += 2) {   // goes from 0 degrees to 180 degrees in steps of 2 deg
+		  waveServo.write(pos);                   // tell servo to go to position in variable 'pos' 
+		  delay(15);                            // waits 15ms for the servo to reach the position 
+	  } 
+	  for(int pos = 180; pos >= 1; pos -= 2) {      // goes from 180 degrees to 0 degrees                           
+		  waveServo.write(pos);                   // tell servo to go to position in variable 'pos' 
+		  delay(15);                            // waits 15ms for the servo to reach the position 
+	  } 
+  }
+	  
+} 
