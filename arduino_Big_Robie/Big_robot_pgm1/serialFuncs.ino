1ae32bddb933849c4f02aa75d85e78aec4e4df06
Paul Bouchier <paul.bouchier@gmail.com>
2025-04-03 23:46:21 -0500

    tweaks to head movement, speed up serial

--- arduino_Big_Robie/Big_robot_pgm1/serialFuncs.ino
+++ arduino_Big_Robie/Big_robot_pgm1/serialFuncs.ino
@@ -7,21 +7,25 @@ void establishContact()
 {
 	while (Serial.available() <= 0)
 	{
-		Serial.println("BIG_ROBOT"); // send string
+		Serial.println("BIG_ROBOT>"); // send string
 		delay(1000);
 	}
 }
 
 int readCmdLine()
 {
-	int recLen = 0;
-	if (Serial.available() > 0)
+	int recLen = Serial.available(); // check for available bytes in the serial buffer
+	if (recLen > bufLen) // if more than the buffer can hold, limit to bufLen
 	{
-		// if there's any serial available, read it:
-		while (Serial.available() > 0)
-		{
-			recLen = Serial.readBytes(inputString, bufLen);
-		}
+		recLen = bufLen; // limit to the buffer size to avoid overflow
 	}
+	else if (recLen <= 0)
+	{
+		return 0; // no data to process
+	}
+	
+	Serial.readBytes(inputString, recLen); // clear out the excess bytes in the buffer
+	inputString[recLen] = '\0';			  // ensure null termination of string
+	Serial.println(inputString);
 	return recLen;
 }
