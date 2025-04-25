b2e2eb0cd80046b01dd4c4305d4a80cb731799b6
Mikew123 <mikew123@gmail.com>
2025-04-25 16:04:41 -0500

    Added head positioning at startup changed loop delay of 0.5 sec to stabilize camera view Added sanning for face when one is not detected fixed periodic arm/wrist motion when a face is detected at center

--- opencv/facedet_test.py
+++ opencv/facedet_test.py
@@ -53,7 +53,7 @@ def getch():
     return ch
 
 #cap = cv2.VideoCapture(0) # Laptop Cam
-cap = cv2.VideoCapture(1) #Robie Cam
+cap = cv2.VideoCapture(1) #Robie Cam windows
 
 # initialize the face recognizer (default face haar cascade)
 #face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
@@ -63,7 +63,7 @@ hasCamera = False
 
 try:    
     _, image = cap.read()
-    print(cap.read())
+#    print(cap.read())
     dim = image.shape
     h = dim[0]
     w = dim[1]
@@ -74,8 +74,47 @@ except:
     print("Unable to open camera")
 
 motion_time_last = time.monotonic()
+do_face_det_motions = True
 
 pose = 5
+# period between make motions when a face is centered
+motion_time_period = 7.0
+
+scan_left = False
+scanlr_cnt = 0
+scanlr_cnt_max = 20
+scan_up = False
+scanud_cnt = 0
+scanud_cnt_max = 10
+
+loop_delay = 0.5
+
+# seems like a delay is needed before interacting with Robie
+time.sleep(2.0)
+
+# position face toward center
+# There is no feedback so go to each stop and back off
+robie_serial.reset_input_buffer()
+for n in range(int(scanud_cnt_max)) :
+    print("D")
+    robie_serial.write("D".encode('ascii'))
+    time.sleep(loop_delay)
+robie_serial.reset_input_buffer()
+for n in range(int(scanud_cnt_max/3)) :
+    print("U")
+    robie_serial.write("U".encode('ascii'))
+    time.sleep(loop_delay)
+robie_serial.reset_input_buffer()
+for n in range(int(scanlr_cnt_max)) :
+    print("L")
+    robie_serial.write("L".encode('ascii'))
+    time.sleep(loop_delay)
+robie_serial.reset_input_buffer()
+for n in range(int(scanlr_cnt_max/2)) :
+    print("R")
+    robie_serial.write("R".encode('ascii'))
+    time.sleep(loop_delay)
+
 while True and hasCamera:
     robie_serial.reset_input_buffer()
     # read the image from the cam
@@ -95,7 +134,6 @@ while True and hasCamera:
             face['x'] = int(x) + width/2
             face['y'] = int(y) + height/2
 
-    #print(dim, face)
     a = face['a']
     x = face['x']
     y = face['y']
@@ -117,24 +155,66 @@ while True and hasCamera:
             react+="C"
 
     # process face status 
-    # L = move head right, D = move head down
-    # C = dont move head it is centered
-    # - = head not detected, maybe go to center after time out
+    # L = move head right, R = move head left
+    # U = move head up, D = move head down
+    # LU, LD, RU RD face detected move head to center it
+    # CC = dont move head the face is centered
+    # "" = face is not detected, scan for a face
     if react!="" :
         if react!="CC" :
             print(react)
             robie_serial.write(react.encode('ascii')) 
-            time.sleep(0.15)
-        else : 
-            print("CC",end="\r")
-        if (time.monotonic() - motion_time_last) > 5.0 :
-            robie_serial.write("WE".encode('ascii'))
-            robie_serial.write(str(pose).encode('ascii'))
-            pose = pose +1
-            if pose == 8 : pose =5
+            do_face_det_motions = True
             motion_time_last = time.monotonic()
+        else : 
+            det_str = "CC"
+            # print("CC",end="\r")
+            if do_face_det_motions :
+                det_str += f"WE{pose}"
+                robie_serial.write("WE".encode('ascii'))
+                robie_serial.write(str(pose).encode('ascii'))
+                pose = pose +1
+                if pose == 9 : pose=5 #poses 5,6,7,8
+            print(det_str)
+            # set timer to make motions every 5 seconds when face is centered
+            if (time.monotonic() - motion_time_last) >  motion_time_period:
+                do_face_det_motions = True
+                motion_time_last = time.monotonic()
+            else :
+                do_face_det_motions = False
     else :
+        # No face detected scan for a face
+        do_face_det_motions = True
         motion_time_last = time.monotonic()
+        scan_str = ""
+        if scanlr_cnt < scanlr_cnt_max :
+            # scan Right and Left
+            scanlr_cnt+=1
+            if scan_left : 
+                scan_str +="L"
+                if scanlr_cnt==scanlr_cnt_max : scan_left=False
+            else : 
+                scan_str +="R"
+                if scanlr_cnt==scanlr_cnt_max : scan_left=True
+        if scanlr_cnt==scanlr_cnt_max :
+            scanlr_cnt = 0  
+            if scanud_cnt < scanud_cnt_max:
+                # scan Up and Down
+                scanud_cnt+=1
+                if scan_up:
+                    scan_str +="U"
+                    if scanud_cnt==scanud_cnt_max : scan_up=False
+                else:
+                    scan_str +="D"
+                    if scanud_cnt==scanud_cnt_max : scan_up=True
+            if scanud_cnt == scanud_cnt_max :
+                scanud_cnt = 0
+
+        print(scan_str)
+        robie_serial.write(scan_str.encode('ascii'))
+
+    # loop rate control
+    time.sleep(loop_delay) 
 
     # if (operating_system=="windows"):
     #     c = getch()
@@ -152,10 +232,6 @@ while True and hasCamera:
 
     if cv2.waitKey(1) == ord("q"):
         break
-
-    #s = robie_serial.readline()
-    #print(s)
-
 
 cap.release()
 cv2.destroyAllWindows()
