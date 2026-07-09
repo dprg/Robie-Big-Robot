67b26165234946b167a7a7d33c9cebb599b6a624
Mikew123 <mikew123@gmail.com>
2026-07-09 18:08:38 -0500

    Restored head movements range to 10 and 20

--- arduino_Big_Robie/Robie_neck_eyes/Robie_neck_eyes.ino
+++ arduino_Big_Robie/Robie_neck_eyes/Robie_neck_eyes.ino
@@ -78,7 +78,7 @@ TMC2209Stepper driver1(&swSerial, R_SENSE, DRIVER1_ADDRESS);
 // stepper motion configuration - does not configure 2290 module
 // TILT Up Down
 #define STEPPER0_TIME_FS		 1000 // full scale travel time ms
-#define STEPPER0_NCMDS_FS    20 // Number of UD commands to travel full scale
+#define STEPPER0_NCMDS_FS    10 // Number of UD commands to travel full scale
 #define STEPPER0_STEPS_FS    50 //34 // full scale range steps not micro steps
 #define STEPPER0_STEPS       (STEPPER0_MICROSTEPS*STEPPER0_STEPS_FS) // micro steps
 #define STEPPER0_STEPDELAY   (1000L*STEPPER0_TIME_FS/(STEPPER0_STEPS)) // usec per microstep
@@ -87,7 +87,7 @@ TMC2209Stepper driver1(&swSerial, R_SENSE, DRIVER1_ADDRESS);
 #define STEPPER0_DDIR        0 // tilt down stepper direction
 // PAN Right Left
 #define STEPPER1_TIME_FS		 2000 // full scale travel time ms
-#define STEPPER1_NCMDS_FS    40 // Number of RL commands to travel full scale
+#define STEPPER1_NCMDS_FS    20 // Number of RL commands to travel full scale
 #define STEPPER1_STEPS_FS    124 // full scale range steps not micro steps
 #define STEPPER1_STEPS       (STEPPER1_MICROSTEPS*STEPPER1_STEPS_FS) // micro steps
 #define STEPPER1_STEPDELAY   (1000L*STEPPER1_TIME_FS/(STEPPER1_STEPS)) // usec per microstep
@@ -440,11 +440,11 @@ static uint32_t last_ms = 0;
 	// for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
 
 	if(panLeftBusy) {
-		pixels.setPixelColor(20, pixels.Color(0, 0, 200));
+		pixels.setPixelColor(7, pixels.Color(0, 0, 200));
 		pixels.show();   // Send the updated pixel colors to the hardware.
 	}
 	else if(panRightBusy) {
-		pixels.setPixelColor(7, pixels.Color(0, 0, 200));
+		pixels.setPixelColor(20, pixels.Color(0, 0, 200));
 		pixels.show();   // Send the updated pixel colors to the hardware.
 	}
 	else if(tiltUpBusy) {
